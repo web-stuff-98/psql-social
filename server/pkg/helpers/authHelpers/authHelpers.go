499e7fb96367916437e64060f5341d6caa724f61
web-stuff-98 <jphoneacc98@gmail.com>
2023-03-28 23:29:14 +0100

    added refresh helper function

--- server/pkg/helpers/authHelpers/authHelpers.go
+++ server/pkg/helpers/authHelpers/authHelpers.go
@@ -19,6 +19,7 @@ func GenerateTokenAndSession(redisClient *redis.Client, ctx context.Context, uid
 			log.Println("Recovered from panic in generate token helper function")
 		}
 	}()
+
 	sid := uuid.New()
 	sessionDuration := time.Minute * 2
 	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
@@ -33,22 +34,42 @@ func GenerateTokenAndSession(redisClient *redis.Client, ctx context.Context, uid
 	return token, err
 }
 
-func GetUidFromToken(redisClient *redis.Client, ctx context.Context, db *pgx.Conn, inToken string) (string, error) {
+func GetUidAndSidFromToken(redisClient *redis.Client, ctx context.Context, db *pgx.Conn, inToken string) (uid string, sid string, err error) {
 	defer func() {
 		if r := recover(); r != nil {
 			log.Println("Recovered from panic in get user ID from session token helper function")
 		}
 	}()
+
 	token, err := jwt.ParseWithClaims(inToken, &jwt.StandardClaims{}, func(t *jwt.Token) (interface{}, error) {
 		return []byte(os.Getenv("SECRET")), nil
 	})
 	sessionID := token.Claims.(*jwt.StandardClaims).Issuer
 	if sessionID == "" {
-		return "", fmt.Errorf("Empty value")
+		return "", "", fmt.Errorf("Empty value")
 	}
 	val, err := redisClient.Get(ctx, sessionID).Result()
 	if err != nil {
-		return "", fmt.Errorf("Error retrieving session")
+		return "", "", fmt.Errorf("Error retrieving session")
 	}
-	return val, nil
+	return val, sessionID, nil
+}
+
+func RefreshToken(redisClient *redis.Client, ctx context.Context, db *pgx.Conn, inToken string) (string, error) {
+	defer func() {
+		if r := recover(); r != nil {
+			log.Println("Recovered from panic in refresh session token helper function")
+		}
+	}()
+
+	if uid, sid, err := GetUidAndSidFromToken(redisClient, ctx, db, inToken); err != nil {
+		return "", err
+	} else {
+		redisClient.Del(ctx, sid)
+		if token, err := GenerateTokenAndSession(redisClient, ctx, uid); err != nil {
+			return "", err
+		} else {
+			return token, nil
+		}
+	}
 }
