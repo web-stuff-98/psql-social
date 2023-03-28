fac0e8b5d8138aee6ef8ad40e16ddbb97f1f7d06
web-stuff-98 <jphoneacc98@gmail.com>
2023-03-28 21:52:00 +0100

    working on socket server

--- server/pkg/socketServer/socketServer.go
+++ server/pkg/socketServer/socketServer.go
@@ -7,17 +7,60 @@ import (
 	"github.com/fasthttp/websocket"
 )
 
+/*
+This works differently to my last 2 projects, mainly because users can
+establish a socket connection without being logged in. Also using
+unidirectional channels for performance.
+
+There are 2 maps for connections. Means more code but also no
+need to go through every connection using range.
+*/
+
 type SocketServer struct {
-	Connections Connections
+	ConnectionsByWs ConnectionsByWs
+	ConnectionsByID ConnectionsByID
 
 	RegisterConn   <-chan ConnnectionData
 	UnregisterConn <-chan *websocket.Conn
+
+	SendDataToUser  <-chan UserMessageData
+	SendDataToConn  <-chan ConnMessageData
+	SendDataToUsers <-chan UsersMessageData
+	SendDataToConns <-chan ConnsMessageData
+
+	JoinSubscriptionByWs  <-chan RegisterUnregisterSubsConnWs
+	JoinSubscriptionByID  <-chan RegisterUnregisterSubsConnID
+	LeaveSubscriptionByWs <-chan RegisterUnregisterSubsConnWs
+	LeaveSubscriptionByID <-chan RegisterUnregisterSubsConnID
+
+	SendDataToSub  <-chan SubscriptionMessageData
+	SendDataToSubs <-chan SubscriptionsMessageData
+	// Send data to subscription, exclude connections
+	SendDataToSubExcludeByWss <-chan SubscriptionMessageDataExcludeByWss
+	// Send data to subscription, exclude connections by user ids
+	SendDataToSubExcludeByIDs <-chan SubscriptionMessageDataExcludeByIDs
+	// Send date to multiple subscriptions, exclude connections
+	SendDataToSubsExcludeByWss <-chan SubscriptionsMessageDataExcludeByWss
+	// Send date to multiple subscriptions, exclude connections by user ids
+	SendDataToSubsExcludeByIDs <-chan SubscriptionMessageDataExcludeByIDs
+
+	Subscriptions Subscriptions
 }
 
 /* ------ MUTEX PROTECTED ------ */
 
-type Connections struct {
+type ConnectionsByWs struct {
 	data  map[*websocket.Conn]string
+	mutex sync.RWMutex
+}
+
+type ConnectionsByID struct {
+	data  map[string]*websocket.Conn
+	mutex sync.RWMutex
+}
+
+type Subscriptions struct {
+	data  map[string]map[*websocket.Conn]struct{}
 	mutex sync.RWMutex
 }
 
@@ -28,54 +71,392 @@ type ConnnectionData struct {
 	Conn *websocket.Conn
 }
 
+type UserMessageData struct {
+	Bytes       []byte
+	Uid         string
+	MessageType int
+}
+
+type UsersMessageData struct {
+	Bytes       []byte
+	Uids        []string
+	MessageType int
+}
+
+type ConnMessageData struct {
+	Bytes       []byte
+	Conn        *websocket.Conn
+	MessageType int
+}
+
+type ConnsMessageData struct {
+	Bytes       []byte
+	Conns       []*websocket.Conn
+	MessageType int
+}
+
+type RegisterUnregisterSubsConnWs struct {
+	Conn    *websocket.Conn
+	SubName string
+}
+
+type RegisterUnregisterSubsConnID struct {
+	Uid     string
+	SubName string
+}
+
+type SubscriptionMessageData struct {
+	SubName     string
+	MessageType int
+	Bytes       []byte
+}
+
+type SubscriptionsMessageData struct {
+	SubNames    []string
+	MessageType int
+	Bytes       []byte
+}
+
 func Init() *SocketServer {
 	ss := &SocketServer{
-		Connections: Connections{
+		ConnectionsByWs: ConnectionsByWs{
 			data: map[*websocket.Conn]string{},
+		},
+		ConnectionsByID: ConnectionsByID{
+			data: make(map[string]*websocket.Conn),
 		},
 
 		RegisterConn:   make(<-chan ConnnectionData),
 		UnregisterConn: make(<-chan *websocket.Conn),
+
+		SendDataToUser:  make(<-chan UserMessageData),
+		SendDataToConn:  make(<-chan ConnMessageData),
+		SendDataToUsers: make(<-chan UsersMessageData),
+		SendDataToConns: make(<-chan ConnsMessageData),
+
+		JoinSubscriptionByWs:  make(<-chan RegisterUnregisterSubsConnWs),
+		JoinSubscriptionByID:  make(<-chan RegisterUnregisterSubsConnID),
+		LeaveSubscriptionByWs: make(<-chan RegisterUnregisterSubsConnWs),
+		LeaveSubscriptionByID: make(<-chan RegisterUnregisterSubsConnID),
+
+		Subscriptions: Subscriptions{
+			data: make(map[string]map[*websocket.Conn]struct{}),
+		},
 	}
 	go runServer(ss)
 	return ss
 }
 
 func runServer(ss *SocketServer) {
-	// Connection registration loop
-	go connectionLoop(ss)
-	// Disconnect registration loop
-	go disconnectLoop(ss)
+	// Connection registration
+	go connection(ss)
+	// Disconnect registration
+	go disconnect(ss)
+	// Send data to user loop
+	go sendUserData(ss)
+	// Send data to a specific connection
+	go sendConnData(ss)
+	// Send data to users loop
+	go sendUsersData(ss)
+	// Send data to connections loop
+	go sendConnsData(ss)
+	// Join connection to subscription by matching websocket
+	go joinSubsByWs(ss)
+	// Join connection subscription by matching user id
+	go joinSubsByID(ss)
+	// Disconnect connection from subscription by matching websocket
+	go leaveSubsByWs(ss)
+	// Disconnect connection from subscription by matching user id
+	go leaveSubsByID(ss)
 }
 
-func connectionLoop(ss *SocketServer) {
+func connection(ss *SocketServer) {
+	var failCount uint8
 	for {
 		defer func() {
 			r := recover()
 			if r != nil {
+				failCount++
 				log.Println("Recovered from panic in ws connection loop:", r)
-				go connectionLoop(ss)
+				if failCount < 10 {
+					go connection(ss)
+				} else {
+					log.Println("Panic recovery count in ws loop exceeded maximum. Loop will not recover.")
+				}
 			}
 		}()
+
 		data := <-ss.RegisterConn
-		ss.Connections.mutex.Lock()
-		ss.Connections.data[data.Conn] = data.Uid
-		ss.Connections.mutex.Unlock()
+		ss.ConnectionsByWs.mutex.Lock()
+		ss.ConnectionsByWs.data[data.Conn] = data.Uid
+		if data.Uid != "" {
+			ss.ConnectionsByID.mutex.Lock()
+			ss.ConnectionsByID.data[data.Uid] = data.Conn
+			ss.ConnectionsByID.mutex.Unlock()
+		}
+		ss.ConnectionsByWs.mutex.Unlock()
 	}
 }
 
-func disconnectLoop(ss *SocketServer) {
+func disconnect(ss *SocketServer) {
+	var failCount uint8
 	for {
 		defer func() {
 			r := recover()
 			if r != nil {
+				failCount++
 				log.Println("Recovered from panic in ws disconnect loop:", r)
-				go disconnectLoop(ss)
+				if failCount < 10 {
+					go disconnect(ss)
+				} else {
+					log.Println("Panic recovery count in ws loop exceeded maximum. Loop will not recover.")
+				}
 			}
 		}()
+
 		conn := <-ss.UnregisterConn
-		ss.Connections.mutex.Lock()
-		delete(ss.Connections.data, conn)
-		ss.Connections.mutex.Unlock()
+		ss.ConnectionsByWs.mutex.Lock()
+		if uid, ok := ss.ConnectionsByWs.data[conn]; ok {
+			ss.ConnectionsByID.mutex.Lock()
+			delete(ss.ConnectionsByID.data, uid)
+			ss.ConnectionsByID.mutex.Unlock()
+		}
+		delete(ss.ConnectionsByWs.data, conn)
+		ss.ConnectionsByWs.mutex.Unlock()
+	}
+}
+
+func sendUserData(ss *SocketServer) {
+	var failCount uint8
+	for {
+		defer func() {
+			r := recover()
+			if r != nil {
+				failCount++
+				log.Println("Recovered from panic in ws send user data loop:", r)
+				if failCount < 10 {
+					go sendUserData(ss)
+				} else {
+					log.Println("Panic recovery count in ws loop exceeded maximum. Loop will not recover.")
+				}
+			}
+		}()
+
+		data := <-ss.SendDataToUser
+		ss.ConnectionsByID.mutex.RLock()
+		// Lock mutex is used to prevent multiple messages being sent to the same connection concurrently
+		if conn, ok := ss.ConnectionsByID.data[data.Uid]; ok {
+			conn.WriteMessage(data.MessageType, data.Bytes)
+		}
+		ss.ConnectionsByID.mutex.RUnlock()
+	}
+}
+
+func sendUsersData(ss *SocketServer) {
+	var failCount uint8
+	for {
+		defer func() {
+			r := recover()
+			if r != nil {
+				failCount++
+				log.Println("Recovered from panic in ws send users data loop:", r)
+				if failCount < 10 {
+					go sendUsersData(ss)
+				} else {
+					log.Println("Panic recovery count in ws loop exceeded maximum. Loop will not recover.")
+				}
+			}
+		}()
+
+		data := <-ss.SendDataToUsers
+		ss.ConnectionsByID.mutex.RLock()
+		for _, v := range data.Uids {
+			if conn, ok := ss.ConnectionsByID.data[v]; ok {
+				conn.WriteMessage(data.MessageType, data.Bytes)
+			}
+		}
+		ss.ConnectionsByID.mutex.RUnlock()
+	}
+}
+
+func sendConnData(ss *SocketServer) {
+	var failCount uint8
+	for {
+		defer func() {
+			r := recover()
+			if r != nil {
+				failCount++
+				log.Println("Recovered from panic in ws send conn data loop:", r)
+				if failCount < 10 {
+					go sendConnData(ss)
+				} else {
+					log.Println("Panic recovery count in ws loop exceeded maximum. Loop will not recover.")
+				}
+			}
+		}()
+
+		data := <-ss.SendDataToConn
+		// Lock mutex is used to prevent multiple messages being sent to the same connection concurrently
+		ss.ConnectionsByWs.mutex.Lock()
+		if _, ok := ss.ConnectionsByWs.data[data.Conn]; ok {
+			data.Conn.WriteMessage(data.MessageType, data.Bytes)
+		}
+		ss.ConnectionsByWs.mutex.Unlock()
+	}
+}
+
+func sendConnsData(ss *SocketServer) {
+	var failCount uint8
+	for {
+		defer func() {
+			r := recover()
+			if r != nil {
+				failCount++
+				log.Println("Recovered from panic in ws send connections data loop:", r)
+				if failCount < 10 {
+					go sendConnsData(ss)
+				} else {
+					log.Println("Panic recovery count in ws loop exceeded maximum. Loop will not recover.")
+				}
+			}
+		}()
+
+		data := <-ss.SendDataToConns
+		ss.ConnectionsByWs.mutex.RLock()
+		for _, conn := range data.Conns {
+			if _, ok := ss.ConnectionsByWs.data[conn]; ok {
+				conn.WriteMessage(data.MessageType, data.Bytes)
+			}
+		}
+		ss.ConnectionsByWs.mutex.RUnlock()
+	}
+}
+
+func joinSubsByWs(ss *SocketServer) {
+	var failCount uint8
+	for {
+		defer func() {
+			r := recover()
+			if r != nil {
+				failCount++
+				log.Println("Recovered from panic in ws register subscription by ws connection loop:", r)
+				if failCount < 10 {
+					go joinSubsByWs(ss)
+				} else {
+					log.Println("Panic recovery count in ws loop exceeded maximum. Loop will not recover.")
+				}
+			}
+		}()
+
+		data := <-ss.JoinSubscriptionByWs
+		ss.Subscriptions.mutex.Lock()
+		if _, ok := ss.Subscriptions.data[data.SubName]; ok {
+			ss.Subscriptions.data[data.SubName][data.Conn] = struct{}{}
+		} else {
+			conns := make(map[*websocket.Conn]struct{})
+			conns[data.Conn] = struct{}{}
+			ss.Subscriptions.data[data.SubName] = conns
+		}
+		ss.Subscriptions.mutex.Unlock()
+	}
+}
+
+func joinSubsByID(ss *SocketServer) {
+	var failCount uint8
+	for {
+		defer func() {
+			r := recover()
+			if r != nil {
+				failCount++
+				log.Println("Recovered from panic in ws register subscription by uid loop:", r)
+				if failCount < 10 {
+					go joinSubsByID(ss)
+				} else {
+					log.Println("Panic recovery count in ws loop exceeded maximum. Loop will not recover.")
+				}
+				go joinSubsByID(ss)
+			}
+		}()
+
+		data := <-ss.JoinSubscriptionByID
+		ss.Subscriptions.mutex.Lock()
+		ss.ConnectionsByID.mutex.RLock()
+		if conn, ok := ss.ConnectionsByID.data[data.Uid]; ok {
+			if _, ok := ss.Subscriptions.data[data.SubName]; ok {
+				ss.Subscriptions.data[data.SubName][conn] = struct{}{}
+			} else {
+				conns := make(map[*websocket.Conn]struct{})
+				conns[conn] = struct{}{}
+				ss.Subscriptions.data[data.SubName] = conns
+			}
+		} else {
+			log.Println("Could not register user ID to subscription - connection information not found in memory")
+		}
+		ss.ConnectionsByID.mutex.RUnlock()
+		ss.Subscriptions.mutex.Unlock()
+	}
+}
+
+func leaveSubsByWs(ss *SocketServer) {
+	var failCount uint8
+	for {
+		defer func() {
+			r := recover()
+			if r != nil {
+				failCount++
+				log.Println("Recovered from panic in ws leave subscription by ws conn loop:", r)
+				if failCount < 10 {
+					go leaveSubsByWs(ss)
+				} else {
+					log.Println("Panic recovery count in ws loop exceeded maximum. Loop will not recover.")
+				}
+				go leaveSubsByWs(ss)
+			}
+		}()
+
+		data := <-ss.LeaveSubscriptionByWs
+		ss.Subscriptions.mutex.RLock()
+		if _, ok := ss.Subscriptions.data[data.SubName]; ok {
+			ss.Subscriptions.mutex.Lock()
+			delete(ss.Subscriptions.data[data.SubName], data.Conn)
+			ss.Subscriptions.mutex.Unlock()
+		}
+		ss.Subscriptions.mutex.RUnlock()
+	}
+}
+
+func leaveSubsByID(ss *SocketServer) {
+	var failCount uint8
+	for {
+		defer func() {
+			r := recover()
+			if r != nil {
+				failCount++
+				log.Println("Recovered from panic in ws leave subscription by uid loop:", r)
+				if failCount < 10 {
+					go leaveSubsByWs(ss)
+				} else {
+					log.Println("Panic recovery count in ws loop exceeded maximum. Loop will not recover.")
+				}
+				go leaveSubsByID(ss)
+			}
+		}()
+
+		data := <-ss.LeaveSubscriptionByID
+		ss.Subscriptions.mutex.RLock()
+		ss.ConnectionsByID.mutex.RLock()
+		conn, connRegistered := ss.ConnectionsByID.data[data.Uid]
+		if !connRegistered {
+			ss.Subscriptions.mutex.RUnlock()
+			ss.ConnectionsByID.mutex.RUnlock()
+			continue
+		}
+		ss.ConnectionsByID.mutex.RUnlock()
+		if _, ok := ss.Subscriptions.data[data.SubName]; ok {
+			ss.Subscriptions.mutex.Lock()
+			delete(ss.Subscriptions.data[data.SubName], conn)
+			ss.Subscriptions.mutex.Unlock()
+		}
+		ss.Subscriptions.mutex.RUnlock()
 	}
 }
