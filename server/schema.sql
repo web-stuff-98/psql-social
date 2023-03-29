933b7366f43fa6ff0242b350c5765bb278ba20ff
web-stuff-98 <jphoneacc98@gmail.com>
2023-03-29 15:40:52 +0100

    fixed sql schema and added new tables

--- server/schema.sql
+++ server/schema.sql
@@ -1,5 +1,3 @@
-DROP DATABASE;
-
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE users (
@@ -8,13 +6,14 @@ CREATE TABLE users (
     password VARCHAR(72) NOT NULL,
     /* "ADMIN" | "USER" */
     role VARCHAR(5) NOT NULL,
-    friends UUID[] DEFAULT '{}'::UUID[],
-    blocked UUID[] DEFAULT '{}'::UUID[]
+    friends UUID [] DEFAULT '{}' :: UUID [],
+    blocked UUID [] DEFAULT '{}' :: UUID []
 );
 
 CREATE TABLE rooms (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    name VARCHAR(24) NOT NULL
+    name VARCHAR(24) NOT NULL,
+    author_id UUID REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE room_channels (
@@ -27,45 +26,103 @@ CREATE TABLE posts (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     title VARCHAR(64) NOT NULL,
     content VARCHAR(5000) NOT NULL,
-    user_id UUID REFERENCES users(id) ON DELETE CASCADE,
+    author_id UUID REFERENCES users(id) ON DELETE CASCADE,
     room_channel_id UUID REFERENCES room_channels(id) ON DELETE CASCADE
 );
 
-CREATE TABLE comments (
+CREATE TABLE room_messages (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    content TEXT NOT NULL,
+    author_id UUID REFERENCES users(id) ON DELETE CASCADE,
+    room_channel_id UUID REFERENCES room_channels(id) ON DELETE CASCADE
+);
+
+CREATE TABLE direct_messages (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    content VARCHAR(200) NOT NULL,
+    author_id UUID REFERENCES users(id) ON DELETE CASCADE,
+    recipient_id UUID REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE post_comments (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     content TEXT NOT NULL,
     post_id UUID REFERENCES posts(id) ON DELETE CASCADE,
-    user_id UUID REFERENCES users(id) ON DELETE CASCADE,
-    parent_comment_id UUID REFERENCES comments(id) ON DELETE CASCADE
+    author_id UUID REFERENCES users(id) ON DELETE CASCADE,
+    parent_comment_id UUID REFERENCES post_comments(id) ON DELETE CASCADE
+);
+
+CREATE TABLE direct_message_attachment_chunks (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    bytes BYTEA NOT NULL,
+    message_id UUID REFERENCES direct_messages(id) ON DELETE CASCADE,
+    next_chunk UUID REFERENCES direct_message_attachment_chunks(id)
+);
+
+CREATE TABLE direct_message_attachment_metadata (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    meta VARCHAR(128) NOT NULL,
+    name VARCHAR(200) NOT NULL,
+    size INT NOT NULL,
+    first_chunk_id UUID REFERENCES direct_message_attachment_chunks(id),
+    message_id UUID REFERENCES direct_messages(id) ON DELETE CASCADE
+);
+
+CREATE TABLE room_message_attachment_chunks (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    bytes BYTEA NOT NULL,
+    message_id UUID REFERENCES room_messages(id) ON DELETE CASCADE,
+    next_chunk UUID REFERENCES room_message_attachment_chunks(id)
+);
+
+CREATE TABLE room_message_attachment_metadata (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    meta VARCHAR(128) NOT NULL,
+    name VARCHAR(200) NOT NULL,
+    size INT NOT NULL,
+    first_chunk_id UUID REFERENCES room_message_attachment_chunks(id),
+    message_id UUID REFERENCES room_messages(id) ON DELETE CASCADE
+);
+
+CREATE TABLE post_comment_attachment_chunks (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    bytes BYTEA NOT NULL,
+    comment_id UUID REFERENCES post_comments(id) ON DELETE CASCADE,
+    next_chunk UUID REFERENCES post_comment_attachment_chunks(id)
+);
+
+CREATE TABLE post_comment_attachment_metadata (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    meta VARCHAR(128) NOT NULL,
+    name VARCHAR(200) NOT NULL,
+    size INT NOT NULL,
+    first_chunk_id UUID REFERENCES post_comment_attachment_chunks(id),
+    comment_id UUID REFERENCES post_comments(id) ON DELETE CASCADE
 );
 
 CREATE TABLE comment_votes (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    user_id UUID REFERENCES users(id) ON DELETE CASCADE,
+    author_id UUID REFERENCES users(id) ON DELETE CASCADE,
     post_id UUID REFERENCES posts(id) ON DELETE CASCADE,
-    comment_id UUID REFERENCES comments(id) ON DELETE CASCADE
+    comment_id UUID REFERENCES post_comments(id) ON DELETE CASCADE
 );
 
 CREATE TABLE post_votes (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    user_id UUID REFERENCES users(id) ON DELETE CASCADE,
-    post_id UUID REFERENCES posts(id) ON DELETE CASCADE,
-);
-
-CREATE TABLE messages (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    content TEXT NOT NULL,
-    user_id UUID REFERENCES users(id) ON DELETE CASCADE,
-    room_channel_id UUID REFERENCES room_channels(id) ON DELETE CASCADE
+    author_id UUID REFERENCES users(id) ON DELETE CASCADE,
+    post_id UUID REFERENCES posts(id) ON DELETE CASCADE
 );
 
 CREATE TABLE profile_pictures (
-  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-  user_id UUID REFERENCES users(id) ON DELETE CASCADE,
-  picture_data BYTEA NOT NULL
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    author_id UUID REFERENCES users(id) ON DELETE CASCADE,
+    picture_data BYTEA NOT NULL
 );
 
 CREATE INDEX idx_username ON users (username);
+
 CREATE INDEX idx_role ON users (role);
+
 CREATE INDEX idx_friends ON users USING gin (friends);
+
 CREATE INDEX idx_blocked ON users USING gin (blocked);
