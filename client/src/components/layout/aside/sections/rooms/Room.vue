8e2973abb6cb357adab0660bd092ab0d5c819203
web-stuff-98 <jphoneacc98@gmail.com>
2023-03-30 21:10:23 +0100

    added intersection observer to room

--- client/src/components/layout/aside/sections/rooms/Room.vue
+++ client/src/components/layout/aside/sections/rooms/Room.vue
@@ -1,9 +1,37 @@
 <script lang="ts" setup>
-defineProps<{ rid: string }>();
+import { computed, onBeforeUnmount, onMounted, ref, toRefs } from "vue";
+import useRoomStore from "../../../../../store/RoomStore";
+
+const props = defineProps<{ rid: string }>();
+
+const { rid } = toRefs(props);
+
+const roomStore = useRoomStore();
+
+const container = ref<HTMLElement>();
+const room = computed(() => roomStore.getRoom(rid.value));
+
+const observer = new IntersectionObserver(([entry]) => {
+  if (entry.isIntersecting) roomStore.roomEnteredView(rid.value);
+  else roomStore.roomLeftView(rid.value);
+});
+
+onMounted(() => {
+  observer.observe(container.value!);
+});
+
+onBeforeUnmount(() => {
+  observer.disconnect();
+});
 </script>
 
 <template>
-  {{ rid }}
+  <div ref="container" class="room">
+    {{ rid }}
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.room {
+}
+</style>
