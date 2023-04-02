8e7aef4fbf736f3e9ccbdcd85fb59dc6a198efe4
web-stuff-98 <jphoneacc98@gmail.com>
2023-04-02 20:39:00 +0100

    forgot something

--- client/src/components/shared/Message.vue
+++ client/src/components/shared/Message.vue
@@ -82,7 +82,7 @@ function handleSubmitEdit(values: any) {
         ...(isAuthor
           ? {}
           : { flexDirection: 'row-reverse', textAlign: 'right' }),
-        ...(true
+        ...(roomId
           ? {
               flexDirection: 'column',
               alignItems: isAuthor ? 'flex-start' : 'flex-end',
